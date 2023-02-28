94af6ef62a9bc7754ca5bc7687b7f1f9957096dd
Christian Omlin <christian.omlin@siemens.com>
2023-02-28 22:08:06 +0100

    feat: add landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingModule } from './pages/landing/landing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, LandingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/pages/landing/landing.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { LandingComponent } from './landing.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LandingComponent],
+  exports: [LandingComponent]
+})
+export class LandingModule { }
